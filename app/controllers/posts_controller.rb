9c39706d5554fa3dd51756c5a95a71be467ba7cf
yuki-o8616 <0ku387431pf3g8d@ezweb.ne.kp>
2020-09-24 18:58:31 +0900

    メモ投稿機能のサーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,11 +4,10 @@ class PostsController < ApplicationController
   end
 
   def create
-    Post.create(content: params[:content])
-    redirect_to action: :index
+    post = Post.create(content: params[:content], checked: false)
+    render json:{ post: post }
   end
   def checked
-    binding.pry
     post = Post.find(params[:id])
     if post.checked 
       post.update(checked: false)
